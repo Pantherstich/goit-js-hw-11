5660612da533014d252c0dbdf478169918936f6a
Pantherstich <pantherstich@gmail.com>
2023-10-14 01:24:09 +0300

    first online

---
+++ src/index.js
@@ -0,0 +1,134 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import Notiflix from 'notiflix';
+import axios from 'axios';
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    close: false,
+    showCounter: false,});
+
+const optionsObserver = {
+    root: null,
+    rootMargin: '200px',
+    threshold: 1.0,
+};
+
+const refs = {
+    searchForm: document.querySelector('.search-form'),
+    gallery: document.querySelector('.gallery'),
+    result: document.querySelector('.res-js')
+};
+
+const API_KEY = '39907244-c493ee587f7162aad68ea1179';
+const BASE_URL = 'https://pixabay.com/api/';
+
+refs.searchForm.addEventListener('submit', onSearch);
+
+let observer = new IntersectionObserver(onLoadMore, optionsObserver);
+let currentPage = 1;
+let searchQuery;
+
+
+async function onLoadMore(entries, observer) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      currentPage += 1;
+
+      fetchPixybay().then(resp =>{
+  Notiflix.notify.info(
+    `Hooray! We found ${resp.data.totalHits} images.`
+  );
+
+  refs.gallery.insertAdjacentHTML('beforeend', createMarkup(resp.data.hits));
+  lightbox.refresh();
+  const { height: cardHeight } =
+            refs.gallery.firstElementChild.getBoundingClientRect();
+
+            window.scrollBy({
+              top: cardHeight * 2,
+              behavior: "smooth",
+            });
+
+            if (currentPage * 40 >= resp.data.totalHits) {
+              observer.unobserve(refs.result);
+            }
+          })
+          .catch(err => console.log(err));
+
+      }
+    })
+}
+
+
+
+async function onSearch (event){
+event.preventDefault();
+let currentPage = 1;
+searchQuery =refs.searchForm.elements.searchQuery.value;
+if (searchQuery.trim() === ''){
+Notiflix.Notify.warning('Emty query, enter your reqest');
+return;
+}
+
+const response = await fetchPixybay ()
+const dataHits = response.data.hits;
+
+if (dataHits.length === 0){
+Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+return;
+}
+refs.gallery.innerHTML = createMarkup(dataHits);
+lightbox.refresh();
+observer.observe(refs.result);
+
+refs.searchForm.reset();
+}
+
+function fetchPixybay (){
+    const params = new URLSearchParams ({
+        key: API_KEY,
+        q: searchQuery,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: 40,
+        page: currentPage,
+    });
+    return axios.get(`${BASE_URL}?${params}`);
+  }
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => 
+      `<div class="photo-card">
+      <a class = "link" href="${largeImageURL}">
+  <img class ="small-img" src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes ${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views ${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments ${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads ${downloads}</b>
+    </p>
+  </div>
+  </a>
+</div>`
+    )
+    .join('');
+}
